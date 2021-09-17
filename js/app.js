e911d009ac42abecad097cf20e1fb328c4078106
Lautaro <lautisuarez081@gmail.com>
2021-09-16 23:08:30 -0300

    Agregando condicionales

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 /* Ingreso de variables */
-let nombre = prompt('¡Bienvenido! Ingrese su nombre: ')
-let num1 = prompt('Ingrese el primer número: ')
-let num2 = prompt('Ingrese el segundo número: ')
+let resultados = null;
+let nombre = prompt('¡Bienvenid@! Ingrese su nombre: ')
+let num1 = Number(prompt('Ingrese el primer número: '))
+let num2 = Number(prompt('Ingrese el segundo número: '))
+let op = prompt('Ingrese la operación que desea hacer: \n( S[sumar], R[restar], M[multiplicar] o D[dividir] )').toLowerCase()
 
-/* Realizamos las operaciones */
-let suma = Number(num1) + Number(num2)
-let resta = Number(num1) - Number(num2)
-let multiplicacion = Number(num1) * Number(num2)
-let division = Number(num1) / Number(num2)
+/* Operaciones y condiciones */
+if (op === 's') {
+    resultados = num1 + num2
+} else if (op === 'r') {
+    resultados = num1 - num2
+} else if (op === 'm') {
+    resultados = num1 * num2
+} else if (op === 'd') {
+    resultados = num1 / num2
+}
 
-/* Mostramos por consola y pantalla */
-alert(`Perfecto ${nombre}! Presione 'aceptar' y puede ver sus resultados por consola ->`)
-console.log('----- RESULTADOS -----')
-console.log(`Suma: ${suma} \nResta: ${resta} \nMultiplicación: ${multiplicacion} \nDivisión: ${division}`)
+/* Mostramos por pantalla */
+if (isNaN(resultados)){ /* Si el usuario ingreso un string en vez de un número el resultado va a ser igual a NaN */
+    alert('Error! Número ingresado incorrecto...')
+} else if (resultados != null){ /* Si el usuario ingreso una letra incorrecta en las operaciones el resultado va a ser igual a null */
+    alert(`Perfecto ${nombre}! \n---------- Resultados ---------- \nTotal: ${resultados}.`)
+} else { 
+    alert('Error! Operación ingresada incorrecta...')
+}
